36af9405dabe1ffb68ef73c92a3f44f004857e71
Pradeeshs14 <pradeeshs@thestackly.com>
2026-05-14 15:49:45 +0530

    react prj1

---
+++ src/components/selfintro.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Selfintro() {
+  return (
+    <div>
+      <h1>Self Introduction</h1>
+
+      <p>Name: Pradeesh</p>
+      <p>Education: BSC IT</p>
+      <p>Skills: React, JavaScript, HTML, CSS</p>
+      <p>Location: Tamil Nadu</p>
+      <p>Career: Full Stack Developer</p>
+    </div>
+  );
+}
+
+export default Selfintro;
